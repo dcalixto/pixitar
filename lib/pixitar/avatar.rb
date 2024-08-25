16137c063f8a021a27c542dd5ad608d97e9afa50
Daniel <dcalixto@protonmail.com>
2024-08-25 17:14:42 -0300

    Update avatar.rb now fully working
    Save the image to the public directory and generate a random number for each avatar created

--- lib/pixitar/avatar.rb
+++ lib/pixitar/avatar.rb
@@ -10,9 +10,9 @@ module Pixitar
 
 
     def initialize(image_class = Pixitar::Image.new, opts = {})
-  @image = image_class.is_a?(Hash) ? Pixitar::Image.new : image_class
-  @assets_path = opts.fetch(:assets_path, Rails.root.join("app", "assets", "images", "pixitar"))
-  @image_extension = opts.fetch(:image_extension, "png")
+       @image = image_class.is_a?(Hash) ? Pixitar::Image.new : image_class
+       @assets_path = opts.fetch(:assets_path, Rails.root.join("app", "assets", "images", "pixitar"))
+       @image_extension = opts.fetch(:image_extension, "png")
     end
 
 
@@ -46,14 +46,8 @@ module Pixitar
       generate_avatar(:female)
     end
 
-   # def generate_avatar(gender = random_gender, filename = "avatar.png")
-     # @gender = gender
-     # raise InvalidGenderError unless genders.include?(gender)
 
-     # create_avatar_image(filename)
-    #end
-
-    def generate_avatar(gender = random_gender, filename = nil)
+def generate_avatar(gender = random_gender, filename = nil)
   @gender = gender
   raise InvalidGenderError unless genders.include?(gender)
 
@@ -68,17 +62,6 @@ end
     private
 
     
-#def create_avatar_image(filename)
- # face_parts.each do |face_part|
- #   asset = random_asset_for(face_part)
- #   image.compose(asset)
- # end
-#  image.save(filename)
-  
-  # Return the file path as a string
-#  File.join(assets_path, filename)
-#end
-
     
 def create_avatar_image(filename)
   face_parts.each do |face_part|
