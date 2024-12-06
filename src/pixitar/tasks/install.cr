f1dba9d0b9028970555e00eeae72834577b2ef59
dcalixto <calixtodaniel@gmail.com>
2024-12-05 23:51:33 -0300

    fixed path

--- src/pixitar/tasks/install.cr
+++ src/pixitar/tasks/install.cr
@@ -7,12 +7,19 @@ module Pixitar
       # Get user's application assets path
       app_assets = File.join(Dir.current, "public/assets/pixitar")
 
+      puts "Installing Pixitar assets..."
+      puts "From: #{shard_assets}"
+      puts "To: #{app_assets}"
+
       # Create directories
       Dir.mkdir_p(app_assets)
 
       # Copy assets if they don't exist
       if File.exists?(shard_assets)
         system("cp -r #{shard_assets}/* #{app_assets}/") unless Dir.entries(app_assets).size > 2
+        puts "Assets installed successfully!"
+      else
+        puts "Warning: Could not find source assets at #{shard_assets}"
       end
     end
   end
